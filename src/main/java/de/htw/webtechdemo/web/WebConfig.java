c5d03eb3f34ab89b687f6d35cab08c8467d34f96
KocakErkay <erkaykocak@gmail.com>
2022-06-22 14:42:45 +0200

    Milestone 4: - WebConfig Annotations hinzugefügt -> kein CORS error mehr

--- src/main/java/de/htw/webtechdemo/web/WebConfig.java
+++ src/main/java/de/htw/webtechdemo/web/WebConfig.java
@@ -1,15 +1,19 @@
-package de.htw.webtechdemo;
+package de.htw.webtechdemo.web;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("http://localhost:3000","https://webtech-ss22-rezeptbuch-580339.herokuapp.com/");
+                .allowedOrigins("http://localhost:3000","https://webtech-frontend-rezeptbuch.herokuapp.com/");
     }
 
     //localhost 3000 ?
